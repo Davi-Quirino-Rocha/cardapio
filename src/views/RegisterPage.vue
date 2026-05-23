a3698e015f1a544dcdc6fb6c0ce5decc66f87013
unknown <Davi>
2026-05-23 12:49:03 -0300

    Mudança no registro

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -35,9 +35,29 @@
         <div class="input-group">
           <label>Senha *</label>
           <input
+            v-model="senha"
             type="password"
             placeholder="Crie uma senha segura"
-          >
+            />
+
+            <p v-if="senha" class="status">
+            {{ statusText }}
+          </p>
+
+          <div class="password-strength">
+            <div
+              v-for="n in 4"
+              :key="n"
+              class="bar"
+              :class="{
+                active:n<=strength,
+                weak:strength<=1,
+                medium:strength>1 && strength<4,
+                strong:strength===4
+              }"
+            />
+
+</div>
         </div>
 
         <div class="input-group">
@@ -66,7 +86,40 @@
   </div>
 
 </template>
- 
+    <script setup>
+  import { ref, computed } from 'vue'
+
+  const senha = ref('')
+
+  const strength = computed(() => {
+  if (!senha.value) return 0
+
+  let score = 0
+
+  // tamanho
+  if (senha.value.length >= 6) score++
+
+  // letras + números
+  if (/[a-zA-Z]/.test(senha.value) && /[0-9]/.test(senha.value))
+    score++
+
+  // maiúscula
+  if (/[A-Z]/.test(senha.value))
+    score++
+
+  // caractere especial
+  if (/[!@#$%^&*(),.?":{}|<>]/.test(senha.value))
+    score++
+
+  return Math.min(score, 4)
+})
+
+  const statusText = computed(() => {
+    if (strength.value <= 1) return 'Senha fraca'
+    if (strength.value <= 3) return 'Senha média'
+    return 'Senha forte'
+  })
+  </script>
 
 <style scoped>
 
@@ -162,4 +215,38 @@ a{
     text-decoration:none;
 }
 
+.password-strength {
+  display: flex;
+  gap: 10px;
+  margin-top: 10px;
+}
+
+.bar {
+  flex: 1;
+  height: 10px;
+  background: #ddd;
+  border-radius: 20px;
+}
+
+.active.weak {
+  background: #d62d2d;
+}
+
+.active.medium {
+  background: orange;
+}
+
+.active.strong {
+  background: #28a745;
+}
+
+.status {
+  color:#d62d2d;
+  font-size:14px;
+  margin-top:8px;
+}
+
+.active.strong{
+  background:#28a745;
+}
 </style>
